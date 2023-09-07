c4151981cf871daa30290088a5012be2b12c6090
makmakcol <141854350+makmakcol@users.noreply.github.com>
2023-09-07 01:24:36 -0500

    Update apiSlice.js

--- src/app/api/apiSlice.js
+++ src/app/api/apiSlice.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { setCredentials } from "../../features/auth/authSlice";
 
 const baseQuery = fetchBaseQuery({
-    baseUrl: 'https://securebank-api.onrender.com',
+    baseUrl: 'https://backend-securebank.onrender.com',
     credentials: 'include',
     prepareHeaders: (headers, { getState }) => {
         const token = getState().auth.token
